0e4c14487c0bc0da4f2a4df4efb7b13ad275b9eb
Marcin <skorekmarcin@gmail.com>
2025-02-23 20:07:33 +0100

    fix: spacing, fonts minor touches

--- src/components/ui/tag.tsx
+++ src/components/ui/tag.tsx
@@ -2,7 +2,7 @@ export const Tag = ({ tag }: { tag: string }) => {
   return (
     <span
       key={tag}
-      className="inline-flex items-center px-3 py-1 text-xs rounded-full 
+      className="inline-flex items-center px-3 py-1 rounded-full 
         bg-accent/10 text-accent-foreground 
         transition-all duration-200 cursor-pointer
         "
