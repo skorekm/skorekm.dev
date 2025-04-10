2f3f2b30de59296265d1908d82ee2c86ad3041ea
Marcin <skorekmarcin@gmail.com>
2025-04-10 17:01:44 +0200

    fix: lets try

--- src/components/ui/mdx-content.tsx
+++ src/components/ui/mdx-content.tsx
@@ -1,10 +1,14 @@
+import { HTMLAttributes } from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import rehypeHighlight from 'rehype-highlight';
 import '@/styles/highlight-js/a11y-dark.css';
 
 export const mdxComponents = {
   pre: ({ children }: { children: React.ReactNode }) => {
-    return <pre tabIndex={0} className="p-0 overflow-y-auto overflow-x-auto">{children}</pre>
+    return <pre className="p-0">{children}</pre>
+  },
+  code: ({ children, ...props }: { children: React.ReactNode, props: HTMLAttributes<HTMLElement> }) => {
+    return <code tabIndex={0} {...props}>{children}</code>
   }
 }
 
